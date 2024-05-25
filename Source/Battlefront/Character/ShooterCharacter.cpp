2f27de5d132df276564dc6936f80357e97e7858c
Shubham More <shubham.a.more20@gmail.com>
2024-05-25 00:05:34 -0600

    CleanUp & Character movement setup

---
+++ Source/Battlefront/Character/ShooterCharacter.cpp
@@ -0,0 +1,83 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ShooterCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+AShooterCharacter::AShooterCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(GetMesh());
+	CameraBoom->TargetArmLength = 600.f;
+	CameraBoom->bUsePawnControlRotation = true;
+
+	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	FollowCamera->bUsePawnControlRotation = false;
+
+}
+
+// Called when the game starts or when spawned
+void AShooterCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AShooterCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AShooterCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ThisClass::Jump);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &ThisClass::moveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ThisClass::moveRight);
+	PlayerInputComponent->BindAxis("Turn", this, &ThisClass::turnAtRate);
+	PlayerInputComponent->BindAxis("LookUp", this, &ThisClass::lookUp);
+	
+}
+
+void AShooterCharacter::moveForward(float value)
+{
+	if (Controller != nullptr && value != 0.f)
+	{
+		const FRotator yawRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+		const FVector direction(FRotationMatrix(yawRotation).GetUnitAxis(EAxis::X));
+		AddMovementInput(direction, value);
+	}
+}
+
+void AShooterCharacter::moveRight(float value)
+{
+	if (Controller != nullptr && value != 0.f)
+	{
+		const FRotator yawRotation(0.f, Controller->GetControlRotation().Yaw, 0.f);
+		const FVector direction(FRotationMatrix(yawRotation).GetUnitAxis(EAxis::Y));
+		AddMovementInput(direction, value);
+	}
+}
+
+void AShooterCharacter::turnAtRate(float value)
+{
+	AddControllerYawInput(value);
+}
+
+void AShooterCharacter::lookUp(float value)
+{
+	AddControllerPitchInput(value);
+}
+
+
---
+++ Source/Battlefront/Character/ShooterCharacter.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "ShooterCharacter.generated.h"
+
+UCLASS()
+class BATTLEFRONT_API AShooterCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AShooterCharacter();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+
+	virtual void BeginPlay() override;
+
+	void moveForward(float value);
+	void moveRight(float value);
+	void turnAtRate(float value);
+	void lookUp(float value);
+
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+	class UCameraComponent* FollowCamera;
+
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+	class USpringArmComponent* CameraBoom;
+
+public:	
+
+};
